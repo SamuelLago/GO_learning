7c6ad34dac5f0e6cdbb9db30b13826bebbe3077d
Samuel <pr0gramaca0apredizad0@gmail.com>
2024-07-13 18:49:03 -0300

    FEAT_slice

---
+++ arrayslicemap/forrange/forrange.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	numeros := [...]int{7, 2, 6, 4, 5, 8} //Compilador conta
+
+	for i, numero := range numeros {
+		fmt.Printf("%d) %d\n", i+1, numero)
+	}
+
+	for _, num := range numeros {
+		fmt.Println(num)
+	}
+}
