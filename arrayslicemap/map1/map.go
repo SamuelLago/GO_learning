6d8d1274716b718f8496cc2add00252e1787350e
Samuel <pr0gramaca0apredizad0@gmail.com>
2024-07-14 22:29:29 -0300

    FEAT_map1

---
+++ arrayslicemap/map1/map.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var aprovados map[int]string
+	// mapas devem ser inicializados
+
+	aprovados := make(map[int]string)
+
+	aprovados[54363732576] = "Maria"
+	aprovados[79134723656] = "João"
+	aprovados[02346572543] = "Pedro"
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados { // cpf == range, nome == aprovados
+		fmt.Printf("%v (CPF: %v)\n", nome, cpf)
+	}
+
+}
