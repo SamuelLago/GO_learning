f278ff6da4053766365dc855af12abb7cabdfc5e
Samuel <pr0gramaca0apredizad0@gmail.com>
2024-07-16 20:44:42 -0300

    FEAT_recursividade

---
+++ funcoes/recursividade/recursividade.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func fatorial(n int) (int, error) {
+	switch {
+	case n < 0:
+		return 0, fmt.Errorf("Numero invalido")
+	case n == 0:
+		return 1, nil
+	default:
+		fatorialAnterior, _ := fatorial(n - 1)
+		return n * fatorialAnterior, nil
+	}
+}
+
+func main() {
+	resultado, _ := fatorial(3)
+	fmt.Println(resultado)
+
+	_, err := fatorial(-4)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
