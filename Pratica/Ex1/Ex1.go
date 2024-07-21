2604a2c513aa3174edb0fbf2b968ae65d2f8ba1a
Samuel <pr0gramaca0apredizad0@gmail.com>
2024-07-20 22:46:38 -0300

    UPDATE_Ex3

--- Pratica/Ex1/Ex1.go
+++ Pratica/Ex1/Ex1.go
@@ -9,7 +9,17 @@ func areaRetangulo(largura, altura float64) float64 { //Ex.2
 	return largura * altura
 }
 
+func operacoesBasicas(num1, num2 float64) string { //Ex.3
+	soma := num1 + num2
+	sub := num1 - num2
+	div := num1 / num2
+	mult := num1 * num2
+
+	return f.Sprintf("Soma:%2.f Subtracao:%2.f Divisao:%2.f Multiplicacao:%2.f", soma, sub, div, mult)
+}
+
 func main() {
 	f.Printf("A temperatura em celsius %2.f°C\n", converteCelsius(91))
 	f.Println("O total da area do retangulo:", areaRetangulo(10, 5))
+	f.Println(operacoesBasicas(4, 2))
 }
