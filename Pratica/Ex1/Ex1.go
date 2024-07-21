389799f1564230bdefad9bde84760f3a66e97622
Samuel <pr0gramaca0apredizad0@gmail.com>
2024-07-20 22:45:06 -0300

    UPDATE_Ex2

--- Pratica/Ex1/Ex1.go
+++ Pratica/Ex1/Ex1.go
@@ -5,7 +5,11 @@ import f "fmt" // Pode mudar o nome do import
 func converteCelsius(temperaturaF float64) float64 { //Ex.1
 	return (temperaturaF - 32) / 1.8
 }
+func areaRetangulo(largura, altura float64) float64 { //Ex.2
+	return largura * altura
+}
 
 func main() {
 	f.Printf("A temperatura em celsius %2.f°C\n", converteCelsius(91))
+	f.Println("O total da area do retangulo:", areaRetangulo(10, 5))
 }
